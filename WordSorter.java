39ff6710ae601cbd37bc8238c3562dd1b4af1514
cesarFarieta <cesarfarietaJr@gmail.com>
2024-10-02 14:17:23 +0000

    still working 10/2

--- WordSorter.java
+++ WordSorter.java
@@ -1,10 +1,20 @@
 import java.io.File;
 import java.util.Scanner;
+import java.util.ArrayList; 
 public class WordSorter 
 {
 	public static void main(String[] args) 
 	{
 		Scanner in = new Scanner(System.in); 
+
+		ArrayList<String>[] wordList =  new ArrayList[26];
+
+		for(int x =0;x<26;x++){
+			wordList[x]=  new ArrayList<String>(); 
+		}// end for loop
+
+
+
 		try
 		{
 			in = new Scanner(new File("article.txt"));	
@@ -14,6 +24,10 @@ public class WordSorter
 			System.out.println("Cannot find file... Exiting Program");
 			return;
 		}	
+
+
+
+
 		String word = "";
 		while(in.hasNext())
 		{
@@ -29,8 +43,21 @@ public class WordSorter
 			word = word.replace("?", "");
 			word = word.replace("'", "");
 			System.out.println(word);
+			word=word.toLowerCase();
 			
 			
 		}
+
+
+		int location;
+
+		for (int x =0 x < word.length();x++){
+			location= word.charAt(x)-97;
+			wordList[location].add(word);
+		}// end for loop 
+		
+
+
 	}
-}
+
+//}
