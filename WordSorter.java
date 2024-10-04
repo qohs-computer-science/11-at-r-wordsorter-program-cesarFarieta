974ef0579ca608e16386bc4dfad9bc2a05da7349
cesarFarieta <cesarfarietaJr@gmail.com>
2024-10-04 14:19:07 +0000

    working 10/4

--- WordSorter.java
+++ WordSorter.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 import java.util.ArrayList; 
 public class WordSorter 
 {
+
+
 	public static void main(String[] args) 
 	{
+
+		boolean keepGoing= true; 
 		Scanner in = new Scanner(System.in); 
 
 		ArrayList<String>[] wordList =  new ArrayList[26];
@@ -45,19 +49,123 @@ public class WordSorter
 			System.out.println(word);
 			word=word.toLowerCase();
 			
-			
+			int curLocation= word.charAt(0)-97;
+			boolean found = false;
+			for (int x =0; x < wordList[curLocation].size();x++){
+				if(word.equals(wordList[curLocation].get(x)))
+					found= true;
+
+				else 
+					found = false;
+
+			}// end for loop
+			if(found = false )	
+				wordList[curLocation].add(word);
+
+		}// end while loop
+
+		while(keepGoing == true){
+			System.out.println("Welcome!  You have the following options:");
+			System.out.println("1- Print out all words starting with a specific letter");
+			System.out.println("2- Print out all words");
+			System.out.println("3- Print out the total number of unique words");
+			System.out.println("4- Search and determine if a word is in the article");
+			System.out.println("5- Remove a word from the data structure");
+			System.out.println("6- Exit");
+			System.out.println("Which type of conversion would you like to perform?");
+			int decision = myObj.nextInt();
+			myObj.nextLine();
+
+			if(decision== 1)
+				wordsWithLetter();
+			else if(decision == 2)
+				printAll();
+			else if(decision == 3)
+				printSize();
+			else if(decision == 4)
+				findWord();
+			else if (decision == 5)
+				removeWord();
+			else{
+				System.out.println("Thank you for coming");
+				keepGoing= false;
+			}
+		}
+	}
+
+	public static void wordsWithLetter(ArrayList<String>[]array){
+		Scanner in = new Scanner(System.in); 
+
+		System.out.println("Please enter a letter");
+		String inputLetter = in.nextLine();
+		int currentLocation = inputLetter.charAt(0)-97;
+
+		for(int x =0;x<array.length;x++){
+
 		}
 
+		System.out.println(array[currentLocation]);
 
-		int location;
+	}
 
-		for (int x =0 x < word.length();x++){
-			location= word.charAt(x)-97;
-			wordList[location].add(word);
-		}// end for loop 
-		
+
+
+	public static void printSize(){
+		int total;
+
+		for(){// start for
+
+
+		}// end for
+
+
+
 
 
 	}
 
-//}
+
+
+	public static void printAll(){
+
+	}
+
+
+
+	public static void findWord(){
+
+	}
+
+
+	public static void binaryToDecimal(){
+
+	}
+
+
+
+	public static void removeWord(){
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
